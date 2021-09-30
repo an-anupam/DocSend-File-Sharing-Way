1339fe4618959fd43ee0e139e147f8c681cbd5c3
an-anupam <anupammohanty522@gmail.com>
2021-10-01 01:40:49 +0530

    CORS UPDATED

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +13,15 @@ app.use(express.static('public'));
 
 const connectDB = require('./config/db');
 connectDB();
+
+//CORS
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  //["http://localhost:3000", "http://localhost:5500", "http://localhost:3500" ]
+}
+
+
+app.use(cors(corsOptions));
 
 //Template Engine
 app.set('views', path.join(__dirname, '/views'));
