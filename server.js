702617a9946b66c804cfb9e734072fecadc9b0c5
an-anupam <anupammohanty522@gmail.com>
2021-10-03 22:16:25 +0530

    k

--- server.js
+++ server.js
@@ -15,11 +15,15 @@ const connectDB = require('./config/db');
 connectDB();
 
 //CORS
-const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-  //["http://localhost:3000", "http://localhost:5000", "http://localhost:3300" ]
-}
+// const corsOptions = {
+//   origin: process.env.ALLOWED_CLIENTS.split(',')
+//   //["http://localhost:3000", "http://localhost:5000", "http://localhost:3300" ]
+// }
 
+
+const corsOptions = {
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 
 app.use(cors(corsOptions));
